ee3af776409f63edbf5a0e7c64ef844ba4a08dec
Sumaiya Akter Shimu <109482577+SumaiyaShimu-483@users.noreply.github.com>
2022-07-21 14:39:35 +0600

    Add files via upload

---
+++ LAB 01/1.483_49-10.cpp
@@ -0,0 +1,18 @@
+//Codefoces Problem NO :4A
+//Problem Name :Watermelon
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int a;
+cout<<"enter your number:";
+cin>>a;
+if(a%2==0&&a>2)
+{
+cout<<"YES";
+}else{
+cout<<"NO";
+}getch();
+}
+
