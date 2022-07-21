e485e93713055a7f7395a07c4db1349f33670136
Sumaiya Akter Shimu <109482577+SumaiyaShimu-483@users.noreply.github.com>
2022-07-21 15:17:40 +0600

    Add files via upload

---
+++ LAB 02/5.483_49-10.cpp
@@ -0,0 +1,13 @@
+//Codefoces Problem NO :50A
+//Problem Name :Domino piling
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ int m,n,p;
+ cout<<"enter your board :";
+ cin>>m>>n;
+ p=(m*n)/2;
+ cout<<p<<endl;
+}
